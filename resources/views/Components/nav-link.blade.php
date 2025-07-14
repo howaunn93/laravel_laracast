07e001804dcf8d914c6786ac0e3d1957a6c87016
howaunn93 <linghowaun@gmail.com>
2025-07-14 19:17:56 +0800

    components

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,1 +1,11 @@
-<x-nav-link href="/" class="{{ request()->is('/') ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</x-nav-link>
+@props(['active' => false])
+
+<a
+    class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium"
+    @if ($active)
+        aria-current="page"
+    @endif
+    {{ $attributes }}
+>
+    {{ $slot }}
+</a>
